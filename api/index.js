d6e4127c7650cd5b8c5f7ea467fda2bdd5a7e2ce
Luke <luke.stephens@trintel.co.za>
2023-03-23 15:52:52 +0200

    test 13

--- api/index.js
+++ api/index.js
@@ -15,16 +15,17 @@ export default async function handler(req, res) {
     const page = await context.newPage()
     await page.goto(link);
 
-    const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3'
+    // const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3'
     const descClass = 'h1._aacl._aaco._aacu._aacx._aad7._aade'
 
-    const image = await page.waitForSelector(imgClass)
-    const imageUrl = await image.getAttribute('src')
-    const description = await page.$eval(descClass, (el) =>
+    // const image = await page.waitForSelector(imgClass)
+    // const imageUrl = await image.getAttribute('src')
+    const description = await page.waitForSelector(descClass)
+    const descriptionText = await page.$eval(descClass, (el) =>
         el.innerText);
 
     await browser.close();
-    res.status(200).json({imageUrl, description});
+    res.status(200).json({description});
   } catch (error) {
     console.error(error);
     res.status(400).json({error: error.message});
