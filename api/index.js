2f93f1ca9d862473c3e3743602361ec057427403
Luke <luke.stephens@trintel.co.za>
2023-03-23 16:18:36 +0200

    node express api

--- api/index.js
+++ api/index.js
@@ -1,6 +1,11 @@
+const express = require('express');
 const playwright = require('playwright-aws-lambda');
 
-export default async function handler(req, res) {
+const app = express();
+
+app.use(express.json());
+
+app.post('/scrape-instagram', async (req, res) => {
   let browser = null;
 
   const link = req.body.link;
@@ -15,19 +20,22 @@ export default async function handler(req, res) {
     const page = await context.newPage()
     await page.goto(link);
 
-    // const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3'
+    const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3'
     const descClass = 'h1._aacl._aaco._aacu._aacx._aad7._aade'
 
-    // const image = await page.waitForSelector(imgClass)
-    // const imageUrl = await image.getAttribute('src')
-    const description = await page.waitForSelector(descClass)
-    const descriptionText = await page.$eval(descClass, (el) =>
-        el.innerText);
+    const image = await page.waitForSelector(imgClass)
+    const imageUrl = await image.getAttribute('src')
+    const desc = await page.waitForSelector(descClass)
+    const descText = desc.innerText()
 
     await browser.close();
-    res.status(200).json({description});
+    res.status(200).json({imageUrl, descText});
   } catch (error) {
     console.error(error);
     res.status(400).json({error: error.message});
   }
-}
+});
+
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
