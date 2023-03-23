7bf3c30c88b3354c22d4ebb50fabcd4c3b1701a7
Luke <luke.stephens@trintel.co.za>
2023-03-23 19:27:59 +0200

    increase timeout

--- index.js
+++ index.js
@@ -18,7 +18,8 @@ app.post('/scrape-instagram', async (req, res) => {
     const context = await browser.newContext();
 
     const page = await context.newPage()
-    await page.goto(link,{waitUntil: "networkidle"});
+    await page.setDefaultTimeout(60000);
+    await page.goto(link, {waitUntil: "networkidle"});
 
     const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3'
     const descClass = 'h1._aacl._aaco._aacu._aacx._aad7._aade'
