2e1e3a511cc74e5b43edeab6f1ec4166b12cb78c
Luke <luke.stephens@trintel.co.za>
2023-03-24 01:07:26 +0200

    try ec2

--- index.js
+++ index.js
@@ -1,42 +1,43 @@
 const express = require('express');
-const playwright = require('playwright')
+const playwright = require('playwright-aws-lambda');
 
 const app = express();
 
-app.use(express.json());
+app.get('/instagram', async (req, res) => {
+  const link = req.query.link;
 
-app.post('/scrape-instagram', async (req, res) => {
-  let browser = null;
-
-  const link = req.body.link;
   if (!link.includes('instagram')) {
-    throw new Error('Not an Instagram link');
+    return res.status(400).json({ error: 'Not an Instagram link' });
   }
 
   try {
-    browser = await playwright.chromium.launch({headless: true,});
+    const browser = await playwright.launchChromium({ headless: true });
     const context = await browser.newContext();
+    const page = await context.newPage();
+    page.setDefaultTimeout(60000);
 
-    const page = await context.newPage()
-    await page.setDefaultTimeout(120000);
-    await page.goto(link, {waitUntil: "networkidle"});
+    await page.goto(link);
 
-    const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3'
-    const descClass = 'h1._aacl._aaco._aacu._aacx._aad7._aade'
+    const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3';
+    const descClass = 'h1._aacl._aaco._aacu._aacx._aad7._aade';
 
-    const image = await page.waitForSelector(imgClass)
-    const imageUrl = await image.getAttribute('src')
-    const desc = await page.waitForSelector(descClass)
-    const descText = await desc.innerText()
+    const image = await page.waitForSelector(imgClass);
+    const imageUrl = await image.getAttribute('src');
+
+    const desc = await page.waitForSelector(descClass);
+    const descText = await desc.innerText();
 
     await browser.close();
-    res.status(200).json({imageUrl, descText});
+
+    return res.status(200).json({ imageUrl, descText });
   } catch (error) {
     console.error(error);
-    res.status(400).json({error: error.message});
+    return res.status(400).json({ error: error.message });
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server listening on port 3000');
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
 });
