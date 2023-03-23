967b8c661fdd53ab732f1a15c2b2ea529cf7d360
Luke <luke.stephens@trintel.co.za>
2023-03-23 15:47:44 +0200

    test 11

--- api/index.js
+++ api/index.js
@@ -13,13 +13,13 @@ export default async function handler(req, res) {
     const context = await browser.newContext();
 
     const page = await context.newPage()
-    await page.goto(link, {waitUntil: "networkidle"});
+    await page.goto(link);
 
     const imgClass = 'img.x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3'
     const descClass = 'h1._aacl._aaco._aacu._aacx._aad7._aade'
 
-    const imageUrl = await page.$eval(imgClass, (el) =>
-        el.getAttribute('src'));
+    const image = await page.waitForSelector(imgClass)
+    const imageUrl = await image.getAttribute('src')
     const description = await page.$eval(descClass, (el) =>
         el.innerText);
 
