cc33ec819c77eb3267c3a9af02195317830ce5f3
Luke <luke.stephens@trintel.co.za>
2023-03-26 10:04:51 +0200

    working aws version

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const playwright = require("playwright-core");
 const chromium = require("@sparticuz/chromium");
 
 exports.handler = async (event, context) => {
+  const {link} = event
+
   const browser = await playwright.chromium.launch({
     args: chromium.args,
     executablePath: await chromium.executablePath(),
@@ -9,14 +11,16 @@ exports.handler = async (event, context) => {
   });
 
   const page = await browser.newPage();
-  await page.goto("https://instagram.com");
-  const pageTitle = await page.title();
-  await browser.close();
+  await page.goto(link,{waitUntil: "networkidle"});
 
-  console.log(`Page title: ${pageTitle}`);
+  const pageTitle = await page.title();
+  const img = await page.$(".x5yr21d.xu96u03.x10l6tqk.x13vifvy.x87ps6o.xh8yej3")
+  const imgUrl = await img.getAttribute('src')
+
+  await browser.close();
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ pageTitle }),
+    body: JSON.stringify({pageTitle, imgUrl}),
   };
 };
